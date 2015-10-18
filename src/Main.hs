5e6b1dc9c99030133ebd6af05a3ef28bccb78446
Steven Williams <steven.m.williams@yandex.com>
2015-10-18 12:00:32 -0400

    Refactored code and added some command options.
    I moved things around to make working with code easier.
    I also changed command options so that not all the account
    info gets viewed on a dump unless you opt for that.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 {- LambdaPass - A password manager using GPGME, JSON and Haskell
    Copyright (C) 2015 Steven Williams
@@ -19,7 +20,9 @@
 
 module Main where
 
-import Control.Monad -- for mzero
+import Types
+import Command.Parser
+
 import Crypto.Gpgme
 import Data.Aeson
 import qualified Data.ByteString as B
@@ -27,52 +30,33 @@ import qualified Data.ByteString.Lazy as BL
 import Data.List
 import Data.String
 import Options.Applicative
+import RCParser (parseRunControl, fileParse)
+import System.Directory (doesFileExist)
 import System.IO
 
-type Filename    = String
-type Fingerprint = String
-type KeyLocation = String
-type Username    = String
-type Password    = String
-type Location    = String
-type Notes       = String
-
--- Option Parser datatypes
-data Command
-    = Add Username Location Notes
-    | View (Maybe Username) (Maybe Location)
-    | Remove (Maybe Username) (Maybe Location)
-
-data Options = Options Filename Fingerprint KeyLocation Command
-
--- JSON file specific datatypes
-data Account = Account { username :: Username
-                       , password :: Password
-                       , location :: Location
-                       , notes    :: Notes
-                       } deriving (Eq, Show)
-
-type Accounts = [Account]
-
-instance FromJSON Account where
-    parseJSON (Object v) = Account <$>
-                           v .: "username" <*>
-                           v .: "password" <*>
-                           v .: "location" <*>
-                           v .: "notes"
-    parseJSON _          = mzero
-
-instance ToJSON Account where
-    toJSON (Account un pw l n) = object $
-                                 [ "username" .= un
-                                 , "password" .= pw
-                                 , "location" .= l
-                                 , "notes"    .= n
-                                 ]
-
 main :: IO ()
-main = run =<< execParser
-       (parseOptions `withInfo` "Password manager using GPG, JSON and most importantly Haskell.")
+main = do
+  exists <- doesFileExist rcPath 
+  case exists of
+    True -> do
+      withRC rcPath
+    False -> do
+      withoutRC
+  where rcPath = "/home/swilliams/.lambdaPassrc"
+        withRC path = do
+          results <- fileParse parseRunControl path
+          case results of
+            Left _ -> putStrLn ("Encountered an error parsing " ++ path) >> withoutRC
+            Right xs -> run =<< execParser
+                        (parseOptionsFromRC (optionsFromRC xs) `withInfo` desc)
+        withoutRC = run =<< execParser
+                    (parseOptions `withInfo` desc)
+        pairExtract x xs = snd . head $ filter (\y -> x == (fst y)) xs
+        optionsFromRC xs = ( pairExtract "file" xs
+                           , pairExtract "fingerprint" xs
+                           , pairExtract "keydir" xs
+                           )
+        desc = "Password manager using GPG, JSON and most importantly Haskell."
 
 -- Command line option parsing
 
@@ -86,70 +70,40 @@ run (Options file fpr key cmd) = do
              p <- getLine
              hSetEcho stdin True
              addNewPassword file fpr key u p l n
-    View u l -> readPassword file key u l
+    ViewAccount u l -> viewAccount file key u l
+    ViewPassword u l -> viewPassword file key u l
     Remove u l -> removePassword file fpr key u l
-
-withInfo :: Parser a -> String -> ParserInfo a
-withInfo opts desc = info (helper <*> opts) $ progDesc desc
-
-parseOptions :: Parser Options
-parseOptions = Options <$> parseFile <*> parseFingerprint <*> parseKey <*> parseCommand
-
-parseFile :: Parser Filename
-parseFile = strOption $
-            short 'f' <> long "file" <> metavar "FILE" <>
-            help "Encrypted file that contains passwords."
-
-parseFingerprint :: Parser Fingerprint
-parseFingerprint = strOption $
-                   short 'p' <> long "fpr" <> metavar "FINGERPRINT" <>
-                   help "Fingerprint for the key you will use to encrypt the passwords file."
-
-parseKey :: Parser KeyLocation
-parseKey = strOption $
-           short 'k' <> long "key" <> metavar "GPGKEY" <>
-           help "This is the path to where your GPG key is."
-
-parseUsername :: Parser (Maybe Username)
-parseUsername = optional $ strOption $ short 'u' <> long "username" <> metavar "USERNAME"
-
-parseLocation :: Parser (Maybe Location)
-parseLocation = optional $ strOption $ short 'l' <> long "location" <> metavar "LOCATION"
-
-parseAdd :: Parser Command
-parseAdd = Add
-           <$> argument str (metavar "USERNAME")
-           <*> argument str (metavar "LOCATION")
-           <*> argument str (metavar "NOTES")
-
-parseRemove :: Parser Command
-parseRemove = Remove
-              <$> parseUsername
-              <*> parseLocation
-
-parseView :: Parser Command
-parseView = View
-            <$> parseUsername
-            <*> parseLocation
-
-parseCommand :: Parser Command
-parseCommand = subparser $
-               command "add" (parseAdd `withInfo` "Add a username, location and password to your datastore.") <>
-               command "view" (parseView `withInfo` "View all accounts.") <>
-               command "remove" (parseRemove `withInfo` "Remove a username and password from the datastor.")
 
 -- Functions for operating on the password file
 
-readPassword :: Filename
-             -> KeyLocation
-             -> Maybe Username
-             -> Maybe Location
-             -> IO ()
-readPassword fn key un loc = do
+viewPassword :: Filename
+                -> KeyLocation
+                -> Maybe Username
+                -> Maybe Location
+                -> IO ()
+viewPassword fn key un loc = do
+             jsonData <- readContents fn key
+             case jsonData of
+               Left (NoData) -> return ()
+               Left x -> decryptErrorHandler x
+               Right plaintext -> do
+                     let accounts = decode $ BL.fromStrict plaintext
+                     let filteredAccounts = accountFiltering un loc accounts accounts
+                     errorCheck filteredAccounts
+                 where f x = password x
+                       errorCheck (Nothing) = return ()
+                       errorCheck (Just xs) = putStrLn . (intercalate "\n\n") $ map f xs
+
+viewAccount :: Filename
+               -> KeyLocation
+               -> Maybe Username
+               -> Maybe Location
+               -> IO ()
+viewAccount fn key un loc = do
   jsonData <- readContents fn key
   case jsonData of
     Left (NoData) -> return ()
-    Left _ -> decryptErrorHandler jsonData
+    Left x -> decryptErrorHandler x
     Right plaintext -> do
                   let accounts = decode $ BL.fromStrict plaintext
                   let filteredAccounts = accountFiltering un loc accounts accounts
@@ -172,11 +126,10 @@ addNewPassword fn fpr key user pass loc note = do
     Left (NoData) -> do
                   let newJSONdata = encode $ appendPassword user pass loc note Nothing
                   writeNewContents fn key fpr newJSONdata
-    Left _ -> decryptErrorHandler jsonData
+    Left x -> decryptErrorHandler x
     Right plaintext -> do
                   let newJSONdata = encode $ appendPassword user pass loc note (decode $ BL.fromStrict plaintext)
                   writeNewContents fn key fpr newJSONdata
-
 
 appendPassword :: Username
                -> Password
@@ -198,17 +151,16 @@ removePassword fn fpr key un loc = do
   jsonData <- readContents fn key
   case jsonData of
     Left (NoData) -> putStrLn "No passwords in file."
-    Left _ -> decryptErrorHandler jsonData
+    Left z -> decryptErrorHandler z
     Right plaintext -> do
                   let accounts = (decode $ BL.fromStrict plaintext)
                   let filteredAccounts = accountFiltering un loc accounts (Just [])
                   let newJSONdata = encode $ (\ufs fs -> filter (\x -> not (x `elem` fs)) ufs) <$> accounts <*> filteredAccounts
                   writeNewContents fn key fpr newJSONdata
 
-decryptErrorHandler :: Either DecryptError Plain -> IO ()
-decryptErrorHandler (Left (BadPass)) = putStrLn "Wrong password. Please enter again."
-decryptErrorHandler (Left _)         = putStrLn "Encountered an unhandled error."
-decryptErrorHandler _                = putStrLn "Something is up with Haskell's pattern matching if you see this."
+decryptErrorHandler :: DecryptError -> IO ()
+decryptErrorHandler (BadPass) = putStrLn "Wrong password. Please enter again."
+decryptErrorHandler _         = putStrLn "Encountered an unhandled error."
 
 accountFiltering :: Maybe Username
                  -> Maybe Location 
